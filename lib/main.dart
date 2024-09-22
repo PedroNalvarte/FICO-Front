7278fe204f8bb4fc8b903a60953e72d874172567
FrancoJess2 <franco_sanchez2@usmp.pe>
2024-09-22 14:55:17 -0500

    diseño de inicio de sesión

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'homepage.dart';
+import 'login_page.dart'; // Importa la nueva pantalla de login
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: LoginPage(), // Cambia para que la primera pantalla sea el login
     );
   }
 }
