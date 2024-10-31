6a3351042877a242e32d7dc4d81f620c3783911d
FrancoJess2 <franco_sanchez2@usmp.pe>
2024-10-31 15:24:25 -0500

    no equipo

--- lib/ListarEventosAdmin.dart
+++ lib/ListarEventosAdmin.dart
@@ -154,136 +154,136 @@ class _ListarEventAdminState extends State<ListarEventAdmin> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        automaticallyImplyLeading: false,
-        toolbarHeight: 150,
-        flexibleSpace: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: Center(
-                      child: Image.asset(
-                        'web/icons/LogoFico.png',
-                        height: 110,
-                      ),
-                    ),
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.notifications, color: Colors.black),
-                    onPressed: () {},
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-      body: SafeArea(
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      automaticallyImplyLeading: false,
+      toolbarHeight: 150,
+      flexibleSpace: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            const SizedBox(height: 10),
             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SizedBox(
-                  width: 200,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromRGBO(158, 17, 15, 1),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      padding: const EdgeInsets.symmetric(vertical: 15),
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        eventosActivos = fetchEventosActivos();
-                      });
-                    },
-                    child: const Text(
-                      'Eventos Publicados',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
+                Expanded(
+                  child: Center(
+                    child: Image.asset(
+                      'web/icons/LogoFico.png',
+                      height: 110,
                     ),
                   ),
                 ),
+                IconButton(
+                  icon: const Icon(Icons.notifications, color: Colors.black),
+                  onPressed: () {},
+                ),
               ],
-            ),
-            const SizedBox(height: 20),
-            Expanded(
-              child: _buildEventosList(),
             ),
           ],
         ),
       ),
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.only(right: 20, bottom: 120),
-        child: Align(
-          alignment: Alignment.bottomRight,
-          child: FloatingActionButton(
-            onPressed: () {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return RegistrarEvento(); // Asegúrate de que esto esté bien definido
-                },
-              );
-            },
-            child: Icon(Icons.add, color: Colors.white),
-            backgroundColor: Color.fromRGBO(158, 17, 15, 1),
-            shape: CircleBorder(),
-          ),
-        ),
-      ),
-      
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(50),
-          child: GNav(
-            onTabChange: (index) {
-              if (index == 3) {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => PerfilAdminPage(emailUsuario: widget.email),
+    ),
+    body: SafeArea(
+      child: Column(
+        children: [
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 200,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromRGBO(158, 17, 15, 1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 15),
                   ),
-                );
-              } else if (index == 1) {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => VisualizarCubiculosAdmin(emailUsuario: widget.email),
+                  onPressed: () {
+                    setState(() {
+                      eventosActivos = fetchEventosActivos();
+                    });
+                  },
+                  child: const Text(
+                    'Eventos Publicados',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                );
-              }
-            },
-            backgroundColor: const Color.fromRGBO(158, 17, 15, 1),
-            color: Colors.white,
-            activeColor: const Color.fromRGBO(158, 17, 15, 1),
-            tabBackgroundColor: Colors.white,
-            gap: 12,
-            padding: const EdgeInsets.all(22),
-            tabs: const [
-              GButton(icon: Icons.calendar_today, text: 'Eventos'),
-              GButton(icon: Icons.meeting_room, text: 'Cubículos'),
-              GButton(icon: Icons.computer, text: 'Equipos'),
-              GButton(icon: Icons.person, text: 'Perfil'),
+                ),
+              ),
             ],
           ),
+          const SizedBox(height: 20),
+          Expanded(
+            child: _buildEventosList(),
+          ),
+        ],
+      ),
+    ),
+    floatingActionButton: Padding(
+      padding: const EdgeInsets.only(right: 20, bottom: 120),
+      child: Align(
+        alignment: Alignment.bottomRight,
+        child: FloatingActionButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return RegistrarEvento(); // Asegúrate de que esto esté bien definido
+              },
+            );
+          },
+          child: Icon(Icons.add, color: Colors.white),
+          backgroundColor: Color.fromRGBO(158, 17, 15, 1),
+          shape: CircleBorder(),
         ),
       ),
-    );
-  }
+    ),
+    
+    bottomNavigationBar: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(50),
+        child: GNav(
+          onTabChange: (index) {
+            if (index == 2) { // Cambié el índice para que coincida con el tercer botón
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => PerfilAdminPage(emailUsuario: widget.email),
+                ),
+              );
+            } else if (index == 1) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => VisualizarCubiculosAdmin(emailUsuario: widget.email),
+                ),
+              );
+            }
+          },
+          backgroundColor: const Color.fromRGBO(158, 17, 15, 1),
+          color: Colors.white,
+          activeColor: const Color.fromRGBO(158, 17, 15, 1),
+          tabBackgroundColor: Colors.white,
+          gap: 12,
+          padding: const EdgeInsets.all(22),
+          tabs: const [
+            GButton(icon: Icons.calendar_today, text: 'Eventos'),
+            GButton(icon: Icons.meeting_room, text: 'Cubículos'),
+            GButton(icon: Icons.person, text: 'Perfil'),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
 }
